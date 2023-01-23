ce6d4b35077d9b3beef0f898b85623c7a1b8d16b
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2023-01-23 18:40:03 -0300

    service de endereço adicionado

--- src/main/java/com/desafiojava/desafiojava/controller/EnderecoController.java
+++ src/main/java/com/desafiojava/desafiojava/controller/EnderecoController.java
@@ -4,28 +4,29 @@ import com.desafiojava.desafiojava.model.Endereco;
 import com.desafiojava.desafiojava.repository.EnderecoRepository;
 import com.desafiojava.desafiojava.service.EnderecoService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class EnderecoController {
 
-    EnderecoService enderecoService;
+    private final EnderecoService enderecoService;
 
     @GetMapping("/endereco")
     public List<Endereco> getAllEnderecos() {
-        return enderecoRepository.findAll();
+        return enderecoService.getAll();
     }
 
     public Optional<Endereco> getEnderecoById(@PathVariable Long id) {
-        return enderecoService.get(id);
+        return Optional.ofNullable(enderecoService.getById(id));
     }
 
     @PostMapping("/endereco")
     public Endereco createEndereco(@RequestBody Endereco endereco) {
-        return enderecoService.save(endereco);
+        return enderecoService.create(endereco);
     }
 }
--- src/main/java/com/desafiojava/desafiojava/service/EnderecoService.java
+++ src/main/java/com/desafiojava/desafiojava/service/EnderecoService.java
@@ -1,8 +1,11 @@
 package com.desafiojava.desafiojava.service;
 
+import com.desafiojava.desafiojava.model.Endereco;
 import com.desafiojava.desafiojava.repository.EnderecoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,9 +13,17 @@ public class EnderecoService {
     private final EnderecoRepository enderecoRepository;
 
     //criar endereco
-
-
+    public Endereco create(Endereco endereco) {
+        return enderecoRepository.save(endereco);
+    }
 
     //listar todos os enderecos
+    public List<Endereco> getAll() {
+        return enderecoRepository.findAll();
+    }
+
     //listar um endereco (principal)
+    public Endereco getById(Long id) {
+        return enderecoRepository.findById(id).get();
+    }
 }
